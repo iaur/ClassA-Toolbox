ca5121b9557f17906b5a62d853c7f1dedec554f3
Fernandez <bernie.lu@outlook.com>
2023-01-31 10:43:37 +0800

    Added take home day 03

--- script/create.ps1
+++ script/create.ps1
@@ -1,8 +1,15 @@
-try{
+try {
     #--init
-    $ErrorActionPreference = "Stop"
+    $global:ErrorActionPreference = "Stop"
     $global:RootPath = split-path -parent $MyInvocation.MyCommand.Definition
-    Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
+    $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
+    $counter = 0 
+
+    function ClearCreateCSV {
+        Remove-Item -Path "$RootPath\create.csv"
+        New-Item $RootPath\create.csv -ItemType File | Out-Null
+        Set-Content $RootPath\create.csv 'Name,Purpose,Members'    
+    }
 
     function Get-Kill {
         param (
@@ -15,24 +22,37 @@ try{
             Write-Output "$(Get-Date -Format "HH:mm")[Error]: Initialization failed at line [$line] due [$e] `n`nwith details `n`n[$msg]`n"
             Write-Output "`n`n------------------END-------------------------"
             Stop-Transcript | Out-Null
+            ClearCreateCSV
             exit
         }else{
             Write-Output "`n`n------------------END-------------------------"
-            Stop-Transcript | Out-Null 
-        }      
+            Stop-Transcript | Out-Null
+            ClearCreateCSV
+            exit
+        }
+        
     }
 
-    $json = Get-Content "$RootPath\config.json" -Raw | ConvertFrom-Json 
-    $CreateCSV = Import-Csv -Path "$RootPath\create.csv"
-    $counter = 0
+    function CheckCreateCSV {
+        $global:CreateCSV = Import-Csv -Path "$RootPath\create.csv"
+        if ($($CreateCSV.Name.Count) -gt 0) {
+            
+        }else{
+            Write-Output "`n------------------OUTPUT($counter)-------------------------"
+            Write-Host "CSV is empty (~_^)" -foregroundcolor Yellow
+            Get-Kill
+        }
+    }
+
+    Start-Transcript -Path "$RootPath\Create_localtime_$(Get-Date -Format "MMddyyyyHHmm").txt" | Out-Null
+    
     Write-Output "`n`n------------------BEGIN-------------------------"
-    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Starting init"
-
-    foreach ($c in $CreateCSV){
+    Write-Output "$(Get-Date -Format "HH:mm")[Log]: Initialization success"
+    CheckCreateCSV
+    foreach($c in $CreateCSV){
         #--transform
-        Write-Output "$(Get-Date -Format "HH:mm")[Log]: Transforming data"
+        Write-Output "`n$(Get-Date -Format "HH:mm")[Log]: Transforming data"
         $NoCharsItem = $(($($c.Name).TrimEnd()).TrimStart())  -replace '[\W]', '' #remove whitespace and special chars
-
         #--assembly
         Write-Output "$(Get-Date -Format "HH:mm")[Log]: Assembling output"
         $AssembledObj = [PSCustomObject]@{
@@ -42,7 +62,6 @@ try{
             Description = "`n Created at: " + $env:COMPUTERNAME + "`n Created by: " + $env:USERNAME + "`n Created on: "  + ($(Get-Date)) + "`n`n=========`n" + $c.Purpose
             Members = ($c.Members) -split (',') #turm members to array
         }
-
         #--output
         $counter++
         Write-Output "`n------------------OUTPUT($counter)-------------------------"
@@ -56,13 +75,8 @@ try{
             Write-Host "No members found" -foregroundcolor Yellow
         }
     }
-
     Get-Kill 
 }
-catch{
+catch {
     Get-Kill -Mode "Hard"
 }
-
-
-
-
